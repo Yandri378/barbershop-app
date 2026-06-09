b09154e8bc03c76e337f246681c15e4b780fc67d
yandri <yandri@gmail.com>
2026-06-09 21:08:23 +0700

    Use external database configuration for Vercel

--- api/index.php
+++ api/index.php
@@ -1,12 +1,3 @@
 <?php
 
-if (getenv('VERCEL') && getenv('DB_CONNECTION') === 'sqlite') {
-    $sourceDatabase = __DIR__ . '/../database/database.sqlite';
-    $runtimeDatabase = '/tmp/database.sqlite';
-
-    if (is_file($sourceDatabase) && ! is_file($runtimeDatabase)) {
-        copy($sourceDatabase, $runtimeDatabase);
-    }
-}
-
 require __DIR__ . '/../public/index.php';
